713352975c7ede14fd303c1b9f0af0fdc9a8dfab
Rick Button <me@rickbutton.me>
2018-08-08 21:15:24 -0500

    removed neovim from profile

--- profile.ps1
+++ profile.ps1
@@ -3,34 +3,6 @@ function reload-profile
 	import-module c:/git/dotfiles/profile.ps1 -force
 }
 
-
-$nvimPath = "C:/git/dotfiles/neovim/bin"
-
-function nvim
-{
-	iex "$nvimPath\nvim-qt -- $args -u C:/git/dotfiles/neovim/config/init.vim"
-}
-
-function nvim-safemode
-{
-	iex "$nvimPath\nvim-qt -- $args"
-}
-
-function nvim-edit-config
-{
-	nvim "C:/git/dotfiles/neovim/config/init.vim"
-}
-
-function nvim-edit-config-safemode
-{
-	nvim-safemode "C:/git/dotfiles/neovim/config/init.vim"
-}
-
-function nvim-edit-ps-profile
-{
-	nvim "C:/git/dotfiles/profile.ps1"
-}
-
 $env:Path += ";C:/git/dotfiles/PortableGit/bin"
 
 import-module c:/git/dotfiles/posh-git/src/posh-git.psm1
