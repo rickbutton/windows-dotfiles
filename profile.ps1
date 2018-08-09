08a8ad5eca3303ece5f382b2bb88226ef71e8bb7
Rick Button <me@rickbutton.me>
2018-08-08 22:31:28 -0500

    minor comment/cleanup

--- profile.ps1
+++ profile.ps1
@@ -3,6 +3,9 @@ $toolsRoot = $dotfilesRoot + "/tools"
 import-module "$dotfilesRoot/dot.psm1"
 import-module "$dotfilesRoot/config.psm1"
 
+# NOTE: modification to these functions will not take until the powershell
+# session is restarted, Reload-Profile will not actually reload these functions
+# this is because this file is sourced, and is not imported as a module that can be removed
 function Reload-Profile() {
 	remove-module "dot"
 	remove-module "config"
@@ -11,7 +14,6 @@ function Reload-Profile() {
 	import-module "$dotfilesRoot/dot.psm1"
 	import-module "$dotfilesRoot/config.psm1"
 }
-
 function Update-Dotfiles() {
 	Reload-Profile
 	$dotfiles = Get-DotFiles
@@ -33,12 +35,13 @@ $env:FZF_DEFAULT_COMMAND = "rg --files --hidden --follow"
 ###### editor
 $env:EDITOR="code --wait"
 
-if (!$env:INIT -and (test-path "c:/git"))
+if (!$env:INIT)
 {
-	set-location "c:/git"
-}
-
-if (!$env:INIT) {
+	if (Test-Path "C:/git")
+	{
+		set-location "c:/git"
+	}
 	"rb powershell profile loaded"
 }
+
 $env:INIT = $TRUE
